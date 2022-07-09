48025010cc955eb3672614b974c40ebc0d681183
Kirill <sonik3000.83@gmail.com>
2022-07-09 18:06:47 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 500;
+
+        int x = 20;
+        int miles = (cost / x);
+
+        System.out.println(miles);
+    }
+}
